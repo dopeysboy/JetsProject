6f9bb464dfee9afa652881b608ce1fb7f8a83b83
Tyler Tanner <tylertanner@Tylers-MacBook-Air.local>
2022-09-02 20:33:29 -0500

    Added method stubs to main application file

--- src/com/skilldistillery/app/JetsApplication.java
+++ src/com/skilldistillery/app/JetsApplication.java
@@ -6,9 +6,60 @@ public class JetsApplication {
 		JetsApplication ja = new JetsApplication();
 		ja.run();
 	}
-	
+
 	public void run() {
-		
+		displayUserMenu();
 	}
 
+	public void readFromFile() {
+		
+	}
+	
+	public void displayUserMenu() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("------------Main Menu------------\n");
+		sb.append("1. List fleet\n");
+		sb.append("2. Fly all jets\n");
+		sb.append("3. View fastest jet\n");
+		sb.append("4. View jet with longest range\n");
+		sb.append("5. Load all Cargo Jets\n");
+		sb.append("6. War in the skies!\n");
+		sb.append("7. Add a jet to Fleet\n");
+		sb.append("8. Remove a jet from Fleet\n");
+		sb.append("9. Quit");
+		
+		System.out.println(sb.toString());
+	}
+	
+	public void listFleet() {
+		
+	}
+	
+	public void flyAll() {
+		
+	}
+	
+	public void viewFastest() {
+		
+	}
+	
+	public void viewFarthest() {
+		
+	}
+	
+	public void loadCargo() {
+		
+	}
+	
+	public void war() {
+		
+	}
+	
+	public void addJet() {
+		
+	}
+	
+	public void removeJet() {
+		
+	}
 }
